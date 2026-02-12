3defdab07f0fb925d8d72625f2ab5d6a8cb6c3c5
Siddique Afridi <siddiqueafridigfx@gmail.com>
2026-02-12 21:45:58 +0500

    path error

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-import { API_BASE_URL } from "../utils/api";
+import { API_BASE_URL } from "./utils/api";
 
 const socket = io(`${API_BASE_URL}`, {
   transports: ["websocket"],
