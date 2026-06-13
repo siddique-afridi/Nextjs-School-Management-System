98bd696db52decf8ece5da62322be0244e509192
Siddique Afridi <siddiqueafridigfx@gmail.com>
2026-06-13 23:44:16 +0500

    restructure: move all files into frontend and backend folders

--- src/socket.js
+++
@@ -1,8 +0,0 @@
-// import { io } from "socket.io-client";
-// import { API_BASE_URL } from "./utils/api";
-
-// const socket = io(`${API_BASE_URL}`, {
-//   transports: ["websocket"],
-// });
-
-// export default socket;
